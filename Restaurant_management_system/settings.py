81fe9ed1e84747bb8debdb55eb87d86790b67fa6
Pedro Lucio Alves Neponuceno <61592753+pedro-alves19@users.noreply.github.com>
2022-09-23 00:05:01 -0300

    Update settings.py

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,3 +144,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'xxxx@gmail.com'   # enter you email address
 EMAIL_HOST_PASSWORD = 'xxxx'     # enter your password 
+
+django_heroku.settings(locals())
